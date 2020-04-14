00c990120c60145b35e41034e3408416efe91538
Marian Babik <marian.babik@cern.ch>
2020-04-14 17:24:13 +0200

    py3 compatibility

--- tests/tests.py
+++ tests/tests.py
@@ -110,16 +110,13 @@ class NAPTests(unittest.TestCase):
         io.status = 0
         io.summary = "summary line"
         io.add_perf_data("cpu", 0.24)
-        io.write("Sample two line output with pipe |\nfrom unit test\n")  # details
+        io.write("Sample two line output\nfrom unit test\n")  # details
         sys.stdout = nap.core.sys_stdout
         print(io.plugin_passive_out())
         print(type(io.plugin_passive_out()))
-        tt = b'PROCESS_SERVICE_CHECK_RESULT;localhost;UnitPlugin;0;OK - summary line | cpu=0.24;;;; ' \
-             b'\\nSample two line output with pipe \u2758\\nfrom unit test\\n'
-        print(type(tt))
 
         self.assertTrue('PROCESS_SERVICE_CHECK_RESULT;localhost;UnitPlugin;0;OK - summary line | cpu=0.24;;;; '
-                        '\\nSample two line output with pipe \u2758\\nfrom unit test\\n' in io.plugin_passive_out())
+                        '\\nSample two line output\\nfrom unit test\\n' in io.plugin_passive_out())
         sys.stdout = nap.core.sys_stdout
 
     def test_subprocess(self):
