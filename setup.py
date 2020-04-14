68efc2abb547c82d0f1c23976b7fc668972d473c
Marian Babik <marian.babik@cern.ch>
2020-04-14 12:58:16 +0200

    updated classifiers

--- setup.py
+++ setup.py
@@ -21,10 +21,9 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.0",
-    "Programming Language :: Python :: 3.1",
-    "Programming Language :: Python :: 3.2",
-    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Topic :: Software Development :: Libraries :: Python Modules"
 ]
 
