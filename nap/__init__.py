31c77035a4ca0f7346efea98432018ab25dcbce6
Marian Babik <marian.babik@cern.ch>
2020-10-05 14:01:49 +0200

    new version 0.1.16

--- nap/__init__.py
+++ nap/__init__.py
@@ -1,7 +1,7 @@
 AUTHOR = "Marian Babik"
 AUTHOR_EMAIL = "<marian.babik@cern.ch>"
 COPYRIGHT = "Copyright (C) 2016 CERN"
-VERSION = "0.1.15"
+VERSION = "0.1.16"
 DATE = "06 Dec 2016"
 __author__ = AUTHOR
 __version__ = VERSION
